f5291f9236b49a0ad58f72189901b84b1c299f89
Camilo Andres <megustalatorta@udenar.edu.co>
2023-09-14 15:53:34 -0500

    Py b1

---
+++ Py/Borrador1.py
@@ -0,0 +1,17 @@
+def Schedule_Class(Num, Classes, Hours, Teachers, Themes):
+    Schedule = []
+
+    index:0
+    while index < len(Schedule_Classes):
+    Scheduled = "The" Classes + 
+    
+    index += 1
+
+
+Schedule_Classes = ["Math", "Lenguage", "Physics", "Chemistry", "Music" ]
+Schedule_Hours = ["7:00", "8:00", "9:00", "10:00", "11:00"]
+Teachers_Classes = ["Mrs. Beth", "Mrs. Jaymes", "Mrs. Kaori", "Mrs. Jude", "Mrs.Taylor"]
+Schedule_Themes = ["Algebra", "Spanish", "1.th law of newton", "Organic" "Gyonopedia"]
+
+print(f"The {Schedule_Num} Class is {Schedule_Classes} at {Schedule_Hours} with {Teachers_Classes} and currently we are studing {Schedule_Themes}")
+
