d29aee7dde578a1aca3bb3723ed9e2c2c2cba368
Camilo Andres <megustalatorta@udenar.edu.co>
2023-09-15 16:02:14 -0500

    Py

--- Py/Borrador1.py
+++ Py/Borrador1.py
@@ -1,19 +1,20 @@
 def Schedule_Class(Num, Classes, Hours, Teachers, Themes):
     Schedule = []
+    index = 0
+    while index < len(Num):
+        Scheduled = "The " + Num[index] + " class is " + Classes[index] + " at " + Hours[index] + " With " + Teachers[index] + " and we're studing " + Themes[index] 
+        Schedule.append(Scheduled)
+        index += 1
 
-    index:0
-    while index < len(Schedule_Classes):
-        Scheduled = "The " + Num + " class is " + Classes + " at " + Hours + " With " + Teachers + " and we're studing " + Themes 
-    index += 1
-
-    return Scheduled
+    return Schedule
 
 
-Schedule_Num = ["1th", "2nd", "3rd", "4th", "5th", "6th"]
-Schedule_Classes = ["Math", "Lenguage", "Physics", "Chemistry", "Music" ]
+Schedule_Num = ["1th", "2nd", "3rd", "4th", "5th"]
+Schedule_Classes = ["Math", "Lenguage", "Physics", "Chemistry", "Music"]
 Schedule_Hours = ["7:00", "8:00", "9:00", "10:00", "11:00"]
 Teachers_Classes = ["Mrs. Beth", "Mrs. Jaymes", "Mrs. Kaori", "Mrs. Jude", "Mrs.Taylor"]
-Schedule_Themes = ["Algebra", "Spanish", "1.th law of newton", "Organic" "Gyonopedia"]
+Schedule_Themes = ["Algebra", "Spanish", "1.th law of newton", "Organic", "Gyonopedia"]
 
-print()
+schedule = Schedule_Class(Schedule_Num, Schedule_Classes, Schedule_Hours, Teachers_Classes, Schedule_Themes)
+print(f"Monday schedule: {schedule}")
 
