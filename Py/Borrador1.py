59f808403e1942c5ae1b19d1425df04ffc1b3982
Camilo Andres <megustalatorta@udenar.edu.co>
2023-09-15 16:00:16 -0500

    Final

--- Py/Borrador1.py
+++ Py/Borrador1.py
@@ -3,15 +3,17 @@ def Schedule_Class(Num, Classes, Hours, Teachers, Themes):
 
     index:0
     while index < len(Schedule_Classes):
-    Scheduled = "The" Classes + 
-    
+        Scheduled = "The " + Num + " class is " + Classes + " at " + Hours + " With " + Teachers + " and we're studing " + Themes 
     index += 1
 
+    return Scheduled
 
+
+Schedule_Num = ["1th", "2nd", "3rd", "4th", "5th", "6th"]
 Schedule_Classes = ["Math", "Lenguage", "Physics", "Chemistry", "Music" ]
 Schedule_Hours = ["7:00", "8:00", "9:00", "10:00", "11:00"]
 Teachers_Classes = ["Mrs. Beth", "Mrs. Jaymes", "Mrs. Kaori", "Mrs. Jude", "Mrs.Taylor"]
 Schedule_Themes = ["Algebra", "Spanish", "1.th law of newton", "Organic" "Gyonopedia"]
 
-print(f"The {Schedule_Num} Class is {Schedule_Classes} at {Schedule_Hours} with {Teachers_Classes} and currently we are studing {Schedule_Themes}")
+print()
 
